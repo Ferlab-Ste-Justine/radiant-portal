6ac250bc42e6662a2f1b5075ad3c26151be601c3
oliviercperrier <olivier.cperrier@hotmail.com>
2025-02-12 13:04:51 -0500

    query-builder wip

--- frontend/themes/tailwind.base.config.js
+++ frontend/themes/tailwind.base.config.js
@@ -3,11 +3,6 @@ module.exports = {
   darkMode: ["class"],
   theme: {
     extend: {
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
